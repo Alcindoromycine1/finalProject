b5e7f2eab5c8640a2ddd44b65edab454ac5d58c9
Alcindoromycine1 <Alcindoromycine1@users.noreply.github.com>
2025-04-01 22:22:54 -0400

    Replace src folder with the new version from C:\Users\akhil\school-eclipse-workspace\lwjglTesting2

--- src/engine/io/Input.java
+++ src/engine/io/Input.java
@@ -6,64 +6,47 @@ import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWMouseButtonCallback;
 
 public class Input {
+	
 	private static boolean[] keys = new boolean[GLFW.GLFW_KEY_LAST];
 	private static boolean[] buttons = new boolean[GLFW.GLFW_MOUSE_BUTTON_LAST];
 	private static double mouseX;
 	private static double mouseY;
-
+	
 	private GLFWKeyCallback keyboard;
 	private GLFWCursorPosCallback mouseMove;
 	private GLFWMouseButtonCallback mouseButtons;
-
+	
 	public Input() {
-
 		keyboard = new GLFWKeyCallback() {
-
-			@Override
 			public void invoke(long window, int key, int scancode, int action, int mods) {
 				keys[key] = (action != GLFW.GLFW_RELEASE);
+				
 			}
 		};
 		mouseMove = new GLFWCursorPosCallback() {
-			public void invoke(long window, double xPos, double yPos) {
-				mouseX = xPos;
-				mouseY = yPos;
+			public void invoke(long window, double xpos, double ypos) {
+				mouseX = xpos;
+				mouseY = ypos;
 			}
 		};
 		mouseButtons = new GLFWMouseButtonCallback() {
 			public void invoke(long window, int button, int action, int mods) {
-				buttons[button] = (action != GLFW.GLFW_RELEASE);
+				buttons[button] = (action != GLFW.GLFW_RELEASE);				
 			}
 		};
 	}
-	/** @return Returns whether the key is being pressed or not*/
-	public static boolean isKeyDown (int key) {
-		
+	
+	public static boolean isKeyDown(int key) {
 		return keys[key];
-		
 	}
-	/** @return Returns whether the mouse is moving or not */
-public static boolean isButtonDown (int button) {
-		
+	public static boolean isButtonDown(int button) {
 		return buttons[button];
-		
 	}
 	
-	/** Clears the keyboard callback*/
 	public void destroy() {
-
 		keyboard.free();
-		mouseButtons.free();
 		mouseMove.free();
-
-	}
-
-	public GLFWKeyCallback getKeyboardCallback() {
-		return keyboard;
-	}
-
-	public void setKeyboard(GLFWKeyCallback keyboard) {
-		this.keyboard = keyboard;
+		mouseButtons.free();
 	}
 
 	public static double getMouseX() {
@@ -74,6 +57,10 @@ public static boolean isButtonDown (int button) {
 		return mouseY;
 	}
 
+	public GLFWKeyCallback getKeyboardCallback() {
+		return keyboard;
+	}
+
 	public GLFWCursorPosCallback getMouseMoveCallback() {
 		return mouseMove;
 	}
@@ -81,5 +68,6 @@ public static boolean isButtonDown (int button) {
 	public GLFWMouseButtonCallback getMouseButtonsCallback() {
 		return mouseButtons;
 	}
-
+	
+	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,92 +1,56 @@
-package project;
+package main;
 
 import org.lwjgl.glfw.GLFW;
 
 import engine.io.Input;
 import engine.io.Window;
 
-public class FinalProject implements Runnable {
+public class Main implements Runnable {
+
 	public Thread game;
-	public static Window window;
-	public static final int WIDTH = 1280;
-	public static final int HEIGHT = 760;
+	public Window window;
+	public final int WIDTH = 1280, HEIGHT = 760;
 
 	public void start() {
-
-		game = new Thread(this, "Final Project");
+		game = new Thread(this, "game");
 		game.start();
-
 	}
 
-	/**
-	 * This method is where you initialize all the objects such as windows, books,
-	 * etc
-	 */
-	public static void init() {
-
-		window = new Window(WIDTH, HEIGHT, "Final Project Title");
-		window.setBackgroundColor(1.0f, 100, 0);
-		window.setFullscreen(true);
+	public void init() {
+		System.out.println("Started Game!");
+		window = new Window(WIDTH, HEIGHT, "Game");
 		window.create();
+
 	}
 
 	public void run() {
 		init();
-
-		final int TARGET_FPS = 60;
-		final long FRAME_TIME = 1000 / TARGET_FPS;
-
-		long lastTime = System.currentTimeMillis();
-		long timer = System.currentTimeMillis();
-		int frames = 0;
-
-		while (!window.close() && !Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) { // Proper way to check if the window should
-																			// close
-			long now = System.currentTimeMillis();
-			long elapsedTime = now - lastTime;
-
-			if (elapsedTime >= FRAME_TIME) {
-				update();
-				render();
-
-				window.swapBuffers();
-
-				lastTime = now;
-				frames++;
-			}
-
-			// FPS counter updates every second
-			if (System.currentTimeMillis() - timer >= 1000) {
-				// Set the window title with FPS
-				window.setTitle("Final Project | FPS: " + frames);
-				frames = 0;
-				timer += 1000;
-			}
-
-			GLFW.glfwPollEvents();
+		while (!window.shouldClose()) {
+			update();
+			render();
+			if (Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) return;
 		}
-		window.destroy();// closes the window
+		window.destroy();
 	}
 
 	private void update() {
-
+		// System.out.println("Updating Game!");
 		window.update();
-		if (Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) {// If the left mouse button is pressed
-
-			System.out.println("X: " + Input.getMouseX() + " , Y: " + Input.getMouseY());
-
+		if (Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) {
+			System.out.println("X: " + Input.getMouseX() + ", Y: " + Input.getMouseY());
 		}
+
 	}
 
 	private void render() {
-
+		// System.out.println("Rendering Game!");
 		window.swapBuffers();
 	}
 
 	public static void main(String[] args) {
 
-		FinalProject a = new FinalProject();
-		a.start();
+		Main m = new Main();
+		m.start();
 
 	}
 
