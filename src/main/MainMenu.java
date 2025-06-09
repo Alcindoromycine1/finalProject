4c1627af2d2d6ba0d26a8d0d8d332968e68e0c7c
The Goat <105932293+thatguywinz@users.noreply.github.com>
2025-06-09 13:40:23 -0400

    easterEgg

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.image.*;
+import java.util.ArrayList;
 
 import java.awt.*;
 import java.io.*;
@@ -15,12 +16,18 @@ public class MainMenu {
 	private boolean hoveringHelp = false;
 	private boolean hoveringExit = false;
 	private boolean hoveringCredits = false;
+	private boolean easterEgg = false;
+	private ArrayList<Character> recommended;
+	String defaultRec = "It is recommended to visit the help menu first.";
 
-	
 	public MainMenu() {
 		loadImages();
+		recommended = new ArrayList<Character>();
+		for (int i = 0; i < defaultRec.length(); i++) {
+			recommended.add(defaultRec.charAt(i));
+		}
 	}
-	
+
 	public void loadImages() {
 		try {
 			background = ImageIO.read(new File("src/textures/mainMenu.png"));
@@ -28,14 +35,40 @@ public class MainMenu {
 			e.printStackTrace();
 		}
 	}
-	
+
+	//insertion sort algorithm
+	//easter egg, sorting the recommended text at the bottom if you click mirror on top right of main menu
+	public String getRecommendation() {
+		if (easterEgg) {
+			for (int i = 1; i < recommended.size(); i++) {
+		        char key = recommended.get(i);
+		        int j = i - 1;
+
+		        while (j >= 0 && recommended.get(j) > key) {
+		            recommended.set(j + 1, recommended.get(j));
+		            j = j - 1;
+		        }
+		        recommended.set(j + 1, key);
+		    }
+			String newRec = "";
+			for (char a : recommended) {
+				newRec += a;
+			}
+			return newRec;
+		} else {
+			return defaultRec;
+		}
+	}
+
 	public void mainMenu(Graphics2D g2) {
 		Color selected = new Color(144, 50, 50);
 		Color unselected = new Color(193, 45, 57);
 		g2.setColor(Color.WHITE);
 		g2.drawImage(background, 0, 0, null);
+		g2.setFont(new Font("DejaVu Math TeX Gyre", Font.BOLD, 14));
+		g2.drawString ("Click me \\/", 680, 70);
 		g2.setFont(new Font("DejaVu Math TeX Gyre", Font.BOLD, 16));
-		g2.drawString("It is recommended to visit the help menu first.", 410, 560);
+		g2.drawString(getRecommendation(), 410, 560);
 		if (hoveringPlay) {
 			g2.setColor(selected);
 			g2.fillRoundRect(245, 240 - 20, 280, 60, 10, 10);
@@ -67,7 +100,7 @@ public class MainMenu {
 			g2.setColor(Color.BLACK);
 			g2.drawRoundRect(245, 240 + 70 - 20, 280, 60, 10, 10);
 			g2.setColor(Color.WHITE);
-			g2.setFont(new Font("DejaVu Math TeX Gyre", Font.BOLD, 24));				   
+			g2.setFont(new Font("DejaVu Math TeX Gyre", Font.BOLD, 24));
 			g2.drawString("Help", 359, 278 + 70 - 20);
 		}
 		if (hoveringCredits) {
@@ -146,4 +179,8 @@ public class MainMenu {
 		this.hoveringCredits = hoveringCredits;
 	}
 	
+	public void setEasterEgg(boolean easterEgg) {
+		this.easterEgg = easterEgg;
+	}
+
 }
